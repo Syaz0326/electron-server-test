6b62ddab3b89aaadd13e9c7753c102bae5f8d9b4
Syaz0326 <tadakazu326@gmail.com>
2018-05-05 14:04:45 +0900

    feature: when db file does not exist, init db and export file

--- database/import.js
+++ database/import.js
@@ -1,11 +1,35 @@
 // sql.js
 const sql = require("sql.js")
 const fs = require("fs")
+let db
 
-let filebuffer = fs.readFileSync('database/data/test.sqlite')
+let filebuffer
 
-const db = new sql.Database(filebuffer)
+// sqliteファイルの作成
+let dbInit = () => {
+  console.log('init')
+  db = new sql.Database()
+  db.run('CREATE TABLE greet (id int, greet char);')
+  let buffer = new Buffer(db.export())
+  fs.writeFileSync('database/data/test.sqlite', buffer)
+}
 
+// sqliteファイルの読み込み
+let importDB = () => {
+  console.log('import')
+  filebuffer = fs.readFileSync('database/data/test.sqlite')
+  db = new sql.Database(filebuffer)
+}
+
+// sqliteファイルの存在チェック
+try {
+  fs.statSync('database/data/test.sqlite')
+  importDB()
+} catch (err) {
+  dbInit()
+}
+
+// select
 let res = db.exec('SELECT * FROM greet')
 
 console.log(res)
