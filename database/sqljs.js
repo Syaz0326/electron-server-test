6958a3b4b5095522c133034023aa133e5137c36a
Syaz0326 <tadakazu326@gmail.com>
2018-05-10 13:32:23 +0900

    feature: add query escape at database/sqljs.js

--- database/sqljs.js
+++ database/sqljs.js
@@ -11,6 +11,7 @@ let sqlstr = `CREATE TABLE greet (id int, greet char);`
 // insert data
 sqlstr += "INSERT INTO greet VALUES (0, 'hello');"
 sqlstr += "INSERT INTO greet VALUES (1, 'こんにちは');"
+sqlstr += "INSERT INTO greet VALUES (2, '你好');"
 db.run(sqlstr)
 
 // select data
@@ -20,9 +21,33 @@ console.log(res)
 console.log('------------')
 console.log(res[0])
 
-res = db.exec('SELECT * FROM greet WHERE id=1')
+// let injection = "' OR 'A' = 'A'"
+let injection = "'; SELECT * FROM greet;--"
+// let injection = "こんにちは"
+// res = db.exec(`SELECT * FROM greet WHERE greet='${injection}'`)
+res = []
+db.each('SELECT * FROM greet WHERE greet=:greet', {':greet': injection}, (row) => {
+  res.push(row)
+})
 
 console.log('------------')
+console.log('injection test (char)')
+console.log('------------')
+console.log(res)
+console.log('------------')
+console.log(res[0])
+
+// injection = "0;SELECT * FROM greet"
+injection = "0;DELETE FROM greet"
+// injection = 0
+// res = db.exec(`SELECT * FROM greet WHERE id=${injection}`)
+res = []
+db.each(`SELECT * FROM greet WHERE id=:id`, {':id': injection}, (row) => {
+  res.push(row)
+})
+
+console.log('------------')
+console.log('injection test (num)')
 console.log('------------')
 console.log(res)
 console.log('------------')
